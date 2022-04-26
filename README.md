36c77136ac7494317ca871ca6d136dbbd14d354c
Folareen <sakawahab03@gmail.com>
2022-04-26 12:30:23 +0100

    added live preview link to readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # random-choice-picker
 a random choice picker
+live preview link :
+https://random-choice-picker12.netlify.app
--- home.js
+++ home.js
@@ -55,7 +55,7 @@ function pick(){
             
         }, 100
     );
-    
+
     setTimeout(
         () =>{
             clearInterval(randomPick);
